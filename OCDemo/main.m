eda77df85438d6c8d28631b85208b55c4fc65986
fucj <491438558@qq.com>
2022-06-08 11:06:59 +0800

    Initial Commit

---
+++ OCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OCDemo
+//
+//  Created by 符传杰 on 2022/6/8.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
